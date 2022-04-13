ed485ce8bef9fae0835dec732d86475b8bedc55a
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2022-04-12 23:51:38 -0500

    awaits

--- src/controllers/places.controller.ts
+++ src/controllers/places.controller.ts
@@ -4,7 +4,7 @@ import { PlacesService } from '../services/place.service'
 export async function getPlaces(req: Request, res: Response): Promise<void> {
   const { page, perPage } = req.query
 
-  const result = PlacesService.getPlaces({
+  const result = await PlacesService.getPlaces({
     page: page ? +page : 1,
     perPage: perPage ? +perPage : 10,
   })
